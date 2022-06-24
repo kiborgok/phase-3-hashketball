b62d90a66792c4046c34b1db1920053795dea0b5
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-24 13:35:20 +0300

    add solution

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,54 @@ def game_hash
 end
 
 # Write code here
+
+def all_players
+  game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+def num_points_scored(player_name)
+  player_stats(player_name)[:points]
+end
+
+def shoe_size(player_name)
+  player_stats(player_name)[:shoe]
+end
+
+def find_team(team_name)
+  # #find on with a hash returns an array with the first key and value that match the condition
+  team_info = game_hash.find do |location, team_data|
+    team_data[:team_name] == team_name
+  end
+
+  # return just the value (team_data) from the .find method
+  team_info[1]
+end
+
+def team_colors(team_name)
+  find_team(team_name)[:colors]
+end
+
+def team_names
+  game_hash.map{|location, team_data| team_data[:team_name]}
+end
+
+def player_numbers(team_name)
+  find_team(team_name)[:players].collect{|player| player[:number]}
+end
+
+def player_stats(player_name)
+  all_players.find{|player| player[:player_name] == player_name}
+end
+
+# helper method to find the player with the biggest shoe
+def big_shoe_player
+  # https://ruby-doc.org/core-3.0.1/Enumerable.html#method-i-max_by
+  all_players.max_by do |player|
+    player[:shoe]
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe_player[:rebounds]
+end
+
